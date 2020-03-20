0a0ec61ec12fb3353a10dfd94cf7a2ebeeec9c07
stCarolas <stcarolas@gmail.com>
2020-03-20 20:34:08 +0300

    update with custom vim hotkeys list

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+conky -c ~/.config/conky/overall -d
+conky -c ~/.config/conky/mem -d
+conky -c ~/.config/conky/clock -d
+conky -c ~/.config/conky/clock_date -d
+conky -c ~/.config/conky/hotkeys -d
+conky -c ~/.config/conky/custom_vim_hotkeys -d
